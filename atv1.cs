81e0d74e94512a2221dac023bc69330c7c292742
ThiagoPScc <141779315+ThiagoPScc@users.noreply.github.com>
2025-01-10 11:12:43 -0300

    Update atv1.cs

--- atv1.cs
+++ atv1.cs
@@ -1,11 +1,24 @@
 using System;
 
-class program{
-
-}
-    static void Main(){
-        int[] vetor = {1,2,5,9,0};
-        int[] vetorInvertido = {};
-
-        Console.WriteLine(vetor);
+class Program
+{
+    static void Main() {
+        int[] vetor = new int[5]{1,2,5,9,0};
+        int[] vetorInvertido = new int[vetor.Length];
+        int iInvert = vetor.Length - 1;
+        foreach (int i in vetor){
+            vetorInvertido[iInvert] = i;
+            iInvert = iInvert - 1;
+        }
+        Console.Write("\n");
+        foreach(int i in vetor){
+        Console.Write(i);
+        }
+         Console.Write("\n");
+        foreach(int i in vetorInvertido){
+            Console.Write(i);
+        }
+        
+        
     }
+}
